6da639efb4c1a4d1921366a420d0c00f4ac61fa1
michjadams <Mjadams66@gmail.com>
2018-01-16 10:01:59 -0500

    initial server

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("new")
+const express = require( 'express' );
+const app = express(); // creates an instance of an express application
+
+app.get('/',function(req,res){
+	res.send("hello there")
+})
+
+
+var listener = app.listen(3000,function(){
+	console.log("listening to port "+ listener.address().port)
+} );
+
+
+	
+
