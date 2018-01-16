a6ef3f8b30598958144f3816eb921b1e9b39fe34
michjadams <Mjadams66@gmail.com>
2018-01-16 10:42:41 -0500

    up to pt5

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+//const morgan = require('morgan')
 
+app.use(function(req,res,next){
+
+	console.log("here "+req.method+" "+ req.path)
+	next()
+
+})
 app.get('/',function(req,res){
 	res.send("hello there")
 })
-
+app.get('/news',function(req,res){
+	res.send("this is the news page")
+})
 
 var listener = app.listen(3000,function(){
 	console.log("listening to port "+ listener.address().port)
